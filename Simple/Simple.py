8aae245beb824afd9b8446de39fd1e8a55de3170
gringorch <leodsanchez89@gmail.com>
2022-02-05 18:46:11 -0300

    Enunciado Ejercicio 1

---
+++ Simple/Simple.py
@@ -0,0 +1,7 @@
+'''
+Hacer una función que genere una lista de diccionarios que contengan id y edad, donde
+edad sea un número aleatorio entre 1 y 100 y la longitud de la lista sea de 10
+elementos. retornar la lista.
+Hacer otra función que reciba lo generado en la primer función y ordenarlo de mayor a
+menor. Printear el id de la persona más joven y más vieja. Devolver la lista ordenada.
+'''
