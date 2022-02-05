a5d92811a92c645f78e1bea6898c894e3124c7d3
gringorch <leodsanchez89@gmail.com>
2022-02-05 19:38:30 -0300

    Ejercicio finalizado

--- Simple/Simple.py
+++ Simple/Simple.py
@@ -5,3 +5,28 @@ elementos. retornar la lista.
 Hacer otra función que reciba lo generado en la primer función y ordenarlo de mayor a
 menor. Printear el id de la persona más joven y más vieja. Devolver la lista ordenada.
 '''
+import random as rd
+
+
+def lista_dict():
+    lista = []
+
+    for i in range(10):
+        edad = rd.randint(1, 100)
+        dict = {}
+        dict = {'ID': i+1, 'Edad': edad}
+        lista.append(dict)
+
+    return lista
+
+
+def ordenar_lista(lista):
+
+    orden = sorted(lista, key=lambda a: a['Edad'], reverse=True)
+
+    print(
+        f'El ID de la persona mas joven es el ID: {orden[-1]["ID"]}, Edad: {orden[-1]["Edad"]}')
+    print(
+        f'El ID de la persona mas vieja es el ID: {orden[0]["ID"]}, Edad: {orden[0]["Edad"]}')
+
+    return orden
